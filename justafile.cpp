3aaaeaee02a158ae578243aefae4bd371130cc75
thiswontworklol <86514248+thiswontworklol@users.noreply.github.com>
2021-07-02 08:26:26 -0700

    ...

---
+++ justafile.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <strings.h>
+
+int main(){
+  std::string name;
+  std::cout << "Enter your name" << '\n';
+  std::cin >> name;
+  std::cout << "Hi" << ' ' << name <<'\n';
+}
